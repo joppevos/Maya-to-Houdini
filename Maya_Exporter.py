7a89a54b8cf74ff373d441ae022722623526c2ec
joppevos <joppe.vos@gmail.com>
2018-10-22 17:57:45 +0200

    Base script for the maya export side

---
+++ Maya_Exporter.py
@@ -0,0 +1,35 @@
+import maya.cmds as cmds
+import json, os
+""" Export the attributes of each light in Maya """
+
+# list the selected lamps in the scene
+lamps = cmds.ls(selection=True)
+
+# attribute keys to place in dict
+attributes = ['scale', 'rotate', 'translate', 'intensity', 'exposure', 'color', 'affectsDiffuse', 'affectsSpecular',
+              'areaVisibleInRender', 'areaBidirectional', 'volumeRayContributionScale']
+
+
+# list of dict with attr keys and lamp in lamps
+def attribute_maker(attributes,lamps):
+    lamp_dict = [{attr: cmds.getAttr('{}.{}'.format(lamp, attr)) for attr in attributes} for lamp in lamps]
+
+    # get the filepath name
+    filepath = cmds.file(q=True, sn=True)
+    filename = os.path.basename(filepath)
+    raw_name, extension = os.path.splitext(filename)
+
+    # add the name of the lamps to lamp_dict
+    for dicts, name in zip(lamp_dict, lamps):
+        dicts['name'] = name
+        dicts['filename'] = raw_name
+    return lamp_dict
+
+json = json.dumps(attribute_maker(attributes,lamps))
+file = open('C:\\Users\\render\\Desktop\\redshiftscript\\rs_mayaLampAttr\\lamp_dict.json', 'w')
+file.write(json)
+file.close()
+
+
+attribute_maker(attributes, lamps)
+
