fa32068a07e6551ef2fff8f28886aa55c41cac0f
joppevos <joppe.vos@gmail.com>
2018-10-23 12:21:28 +0200

    added the option to enter a file path raises an error if no lights are selected

--- Maya_Exporter.py
+++ Maya_Exporter.py
@@ -1,9 +1,19 @@
 import maya.cmds as cmds
 import json, os
+
 """ Export the attributes of each light in Maya """
 
-# list the selected lamps in the scene
-lamps = cmds.ls(selection=True)
+
+# list the selected lamps in the
+def list_lamps():
+    lamps = cmds.ls(selection=True)
+    if lamps == []:
+        cmds.confirmDialog(title='Confirm', message='Please select any Lights')
+        error = ValueError('No lights selected')
+        raise error
+    else:
+        return lamps
+
 
 # attribute keys to place in dict
 attributes = ['scale', 'rotate', 'translate', 'intensity', 'exposure', 'color', 'affectsDiffuse', 'affectsSpecular',
@@ -11,29 +21,50 @@ attributes = ['scale', 'rotate', 'translate', 'intensity', 'exposure', 'color', 
 
 
 # list of dict with attr keys and lamp in lamps
-def attribute_maker(attributes,lamps):
+def attribute_maker(attributes, lamps):
     lamp_dict = [{attr: cmds.getAttr('{}.{}'.format(lamp, attr)) for attr in attributes} for lamp in lamps]
 
-    # get the filepath name
+    # get the scene name of maya
     filepath = cmds.file(q=True, sn=True)
     filename = os.path.basename(filepath)
     raw_name, extension = os.path.splitext(filename)
 
-    # add the name of the lamps to lamp_dict
+    # add the name of the lamps to lamp_dict and the filename of scene to dict
     for dicts, name in zip(lamp_dict, lamps):
         dicts['name'] = name
         dicts['filename'] = raw_name
     return lamp_dict
 
 
-# TODO: WRAP IN FUNCTION
-json = json.dumps(attribute_maker(attributes,lamps))
-file = open('C:\\Users\\render\\Desktop\\redshiftscript\\rs_mayaLampAttr\\lamp_dict.json', 'w')
-file.write(json)
-file.close()
+def filepath():
+    """ ask user for local file path to save and returns the give path"""
+    result = cmds.promptDialog(
+        title='Rename Object',
+        message=" Enter a 'LOCAL' path (C:\\Users\\render\\examplename):  ",
+        button=['OK', 'Cancel'],
+        defaultButton='OK',
+        cancelButton='Cancel',
+        dismissString='Cancel')
 
-# TODO: WRAP IN TRY IF NO LIGHTS ARE SELECTED ERROR,
+    if result == 'OK':
+        text = cmds.promptDialog(query=True, text=True)
+        return text
+
+
+def write_attributes(attrdict):
+    """ Write out the attributes in json"""
+    filename = filepath()
+    file = open('{}.json'.format(filename), 'w')
+    print(file)
+    file.write(attrdict)
+    file.close()
+    cmds.confirmDialog(title='LightExporter', message='Lights have been exported')
+
+
+json = json.dumps(attribute_maker(attributes, list_lamps()))
+
+write_attributes(json)
+
 # AND FINALLY GIVE MESSAGE TO USER 'LIGHT DATA HAS BEEN EXPORTED!"
 
-attribute_maker(attributes, lamps)
 
