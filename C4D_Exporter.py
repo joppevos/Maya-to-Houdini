192ca3b41d9319d1280d199e6584c2ead1542c6d
joppevos <joppe.vos@gmail.com>
2018-11-23 17:54:58 +0100

    started

--- C4D_Exporter.py
+++ C4D_Exporter.py
@@ -10,3 +10,21 @@ def main():
     c4d.EventAdd()
 
 main()
+
+
+import c4d
+#Welcome to the world of Python
+
+def rand():
+    import random
+    x = random.randrange(1,1000,1)
+    return x
+
+def update():
+    obj = doc.GetObjects()
+    v = c4d.Vector(rand(),rand(), rand())
+    obj[0].SetRelPos(v)
+
+def main():
+    if frame%10:
+        update()
